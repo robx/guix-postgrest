8e2672d171e4a03c4758e6687397ae0c56e62048
Robert Vollmert <rob@vllmrt.net>
2019-06-07 12:20:28 +0200

    add db-url, db-schema, db-anon-role postgrest options

--- service.scm
+++ service.scm
@@ -23,18 +23,26 @@
 (define-record-type* <postgrest-configuration>
   postgrest-configuration make-postgrest-configuration
   postgrest-configuration?
-  (postgrest   postgrest-configuration-postgrest ;<package>
-               (default postgrest))
-  (server-host postgrest-configuration-server-host
-               (default "127.0.0.1"))
-  (server-port postgrest-configuration-server-port
-               (default 3000))
-  ; postgres port, etc.
-  (config-file postgrest-configuration-config-file
-               (default #f)))
+  (postgrest     postgrest-configuration-postgrest ;<package>
+                 (default postgrest))
+  (db-uri        postgrest-configuration-db-uri
+                 (default "postgres://postgrest@localhost:5432/postgrestdb"))
+  (db-schema     postgrest-configuration-db-schema
+                 (default "public"))
+  (db-anon-role  postgrest-configuration-db-anon-role
+                 (default "postgrest-anon"))
+  (server-host   postgrest-configuration-server-host
+                 (default "127.0.0.1"))
+  (server-port   postgrest-configuration-server-port
+                 (default 3000))
+  (config-file   postgrest-configuration-config-file
+                 (default #f)))
 
-(define (default-postgrest.conf server-host server-port)
+(define (default-postgrest.conf db-uri db-schema db-anon-role server-host server-port)
   (mixed-text-file "postgrest.conf"
+                   "db-uri = \"" db-uri "\"\n"
+                   "db-schema = \"" db-schema "\"\n"
+                   "db-anon-role = \"" db-anon-role "\"\n"
                    "server-host = \"" server-host "\"\n"
                    "server-port = " (number->string server-port) "\n"))
 
@@ -50,10 +58,12 @@
 
 (define postgrest-shepherd-service
   (match-lambda
-    (($ <postgrest-configuration> postgrest server-port server-host config-file)
+    (($ <postgrest-configuration> postgrest db-uri db-schema db-anon-role
+                                  server-port server-host config-file)
      (let ((config-file
             (or config-file
-              (default-postgrest.conf server-port server-host))))
+              (default-postgrest.conf db-uri db-schema db-anon-role
+                                      server-port server-host))))
        (list (shepherd-service
               (provision '(postgrest))
               (documentation "Run the PostgREST daemon.")
